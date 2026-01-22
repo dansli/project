e49565e42a70b877f9110e0c6c4f785d1d852883
dansli <dansli@live.com>
2026-01-22 17:04:43 +0800

    update sql

--- 机器人/国内机器人/行权限.sql
+++ 机器人/国内机器人/行权限.sql
@@ -8,6 +8,7 @@ and MANAGER_USERID=USER_ID
 and LEVEL6_ORG_CODE is null -- 大区
 and LEVEL6_ORG_CODE is not null -- 督导
 
+
 insert into dw.dim_cn_robo_rowauth
 select 
   USER_ID,
@@ -27,9 +28,10 @@ from dw.dim_fine_user_department_mapping as u
 left join dw.dim_fine_department as d on u.DEPT_ID=d.DEPT_ID
 left join dw.dim_mannual_robo_warehouse_info as w on w.ctrer=d.LEVEL5_ORG_NAME
 left join 
-(select city_wrh_code,robot_code,max(snap_datetime) 
+(select distinct city_wrh_code,robot_code
 from dw.api_hby_robot_operator_snap 
-group by city_wrh_code,robot_code) as r  
+where store_status='正常' and snap_datetime = date_sub(CURDATE(), interval 1 day)
+  ) as r  
 on r.city_wrh_code = w.wrh_code
 where 
 d.LEVEL2_ORG_CODE='100636' and LEVEL3_ORG_CODE='100056' and LEVEL4_ORG_CODE='100057'
@@ -54,9 +56,10 @@ from dw.dim_fine_user_department_mapping as u
 left join dw.dim_fine_department as d on u.DEPT_ID=d.DEPT_ID
 left join dw.dim_mannual_robo_warehouse_info as w on w.sup_email = u.email
 left join 
-(select city_wrh_code,robot_code,max(snap_datetime) 
+(select distinct city_wrh_code,robot_code
 from dw.api_hby_robot_operator_snap 
-group by city_wrh_code,robot_code) as r  
+where store_status='正常' and snap_datetime = date_sub(CURDATE(), interval 1 day)
+  ) as r  
 on r.city_wrh_code = w.wrh_code
 where 
 d.LEVEL2_ORG_CODE='100636' and LEVEL3_ORG_CODE='100056' and LEVEL4_ORG_CODE='100057'
@@ -102,10 +105,111 @@ left join dw.dim_fine_user_department_mapping as u
 on u.email = w.email
 left join dw.dim_fine_department as d on u.DEPT_ID=d.DEPT_ID
 left join 
-(select city_wrh_code,robot_code,max(snap_datetime) 
+(select distinct city_wrh_code,robot_code
 from dw.api_hby_robot_operator_snap 
-group by city_wrh_code,robot_code) as r  
+where store_status='正常' and snap_datetime = date_sub(CURDATE(), interval 1 day)
+  ) as r  
 on r.city_wrh_code = w.wrh_code
+where USER_ID is not null
+
+
+
+insert into dw.dim_cn_robo_rowauth
+select 
+  USER_ID,
+  USER_NAME,
+  u.EMAIL,
+  LEVEL1_ORG_NAME,
+  LEVEL2_ORG_NAME,
+  LEVEL3_ORG_NAME,
+  LEVEL4_ORG_NAME,
+  LEVEL5_ORG_NAME,
+  LEVEL6_ORG_NAME,
+  '机器人渠道部-大区' as role,
+  w.wrh_code,
+  w.wrh_name,
+  hr.robot_code
+from dw.dim_fine_user_department_mapping as u 
+left join dw.dim_fine_department as d on u.DEPT_ID=d.DEPT_ID
+left join dw.dim_mannual_robo_warehouse_info as w on w.ctrer=d.LEVEL5_ORG_NAME
+left join dw.api_hby_robot_operator_snapshot as hr
+on hr.ctrer=d.LEVEL5_ORG_NAME and hr.snapshot_date=date_sub(current_date(),1)
+where 
+d.LEVEL2_ORG_CODE='100636' and LEVEL3_ORG_CODE='100056' and LEVEL4_ORG_CODE='100057'
+and MANAGER_USERID=USER_ID
+and ORG_LEVEL='5'
+and hr.store_status='正常'
+union 
+select 
+  u.USER_ID,
+  u.USER_NAME,
+  u.EMAIL,
+  LEVEL1_ORG_NAME,
+  LEVEL2_ORG_NAME,
+  LEVEL3_ORG_NAME,
+  LEVEL4_ORG_NAME,
+  LEVEL5_ORG_NAME,
+  LEVEL6_ORG_NAME,
+  '机器人渠道部-督导' as role,
+  w.wrh_code,
+  w.wrh_name,
+  hr.robot_code
+from dw.dim_fine_user_department_mapping as u 
+left join dw.dim_fine_department as d on u.DEPT_ID=d.DEPT_ID
+left join dw.dim_mannual_robo_warehouse_info as w on w.sup_email = u.email
+left join dw.api_hby_user_snapshot as hu 
+on hu.fs_user_id=u.USER_ID and hu.snapshot_date=date_sub(current_date(),1)
+left join dw.api_hby_robot_operator_snapshot as hr 
+on hr.sprvsr_id=hu.hby_user_id and hr.snapshot_date=date_sub(current_date(),1)
+where 
+d.LEVEL2_ORG_CODE='100636' and LEVEL3_ORG_CODE='100056' and LEVEL4_ORG_CODE='100057'
+and MANAGER_USERID=USER_ID
+and ORG_LEVEL='6'
+and hr.store_status='正常'
+union 
+select 
+  USER_ID,
+  USER_NAME,
+  u.EMAIL,
+  LEVEL1_ORG_NAME,
+  LEVEL2_ORG_NAME,
+  LEVEL3_ORG_NAME,
+  LEVEL4_ORG_NAME,
+  LEVEL5_ORG_NAME,
+  LEVEL6_ORG_NAME,
+  '机器人渠道部-总部' as role,
+  'ALL' as wrh_code,
+  'ALL' as wrh_name,
+  'ALL' as robot_code
+from dw.dim_fine_user_department_mapping as u 
+left join dw.dim_fine_department as d on u.DEPT_ID=d.DEPT_ID
+where 
+d.LEVEL2_ORG_CODE='100636' and LEVEL3_ORG_CODE='100056' and ORG_LEVEL in ('3','4')
+union 
+select 
+distinct 
+  USER_ID,
+  USER_NAME,
+  u.EMAIL,
+  LEVEL1_ORG_NAME,
+  LEVEL2_ORG_NAME,
+  LEVEL3_ORG_NAME,
+  LEVEL4_ORG_NAME,
+  LEVEL5_ORG_NAME,
+  LEVEL6_ORG_NAME,
+  '机器人渠道部-库管' as role,
+  w.wrh_code,
+  w.wrh_name,
+  null as robot_code
+from dw.dim_mannual_robo_warehouse_info as w
+left join dw.dim_fine_user_department_mapping as u 
+on u.email = w.email
+left join dw.dim_fine_department as d on u.DEPT_ID=d.DEPT_ID
+-- left join 
+-- (select city_wrh_code,robot_code,max(snap_datetime) 
+-- from dw.api_hby_robot_operator_snap 
+-- group by city_wrh_code,robot_code) as r  
+-- on r.city_wrh_code = w.wrh_code
 where USER_ID is not null
 
 
@@ -195,3 +299,15 @@ VALUES (
   :role,
   :department,
   :update_time)
+
+
+
+
+  select 
+  role.name as role_name, 
+  role.id as role_id,
+  user.userId as user_id_fr
+from fine61db.FINE_CUSTOM_ROLE as role 
+left join fine61db.FINE_USER_ROLE_MIDDLE as user 
+on role.id=user.roleId
+where role.id in ('15ed6386-e9b9-492f-becb-0af89d999816','4e0ca643-20d1-4348-ad91-157e7fd7cac6','6e11e1fa-0db8-4b75-b269-558543e19f4e','7502a278-e710-4a97-a1e4-007144b86a06')
